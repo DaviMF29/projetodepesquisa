745d6444c1ff252fda0848089dc855aab5af7018
DaviMF29 <migueldavi313@gmail.com>
2024-08-07 15:57:21 -0300

    feat: Add delete group functionality

--- controllers/group_controller.py
+++ controllers/group_controller.py
@@ -79,3 +79,18 @@ def get_students_from_group_controller(id_group):
     connection = db_connection()
     teacher,students = Group.get_students_from_group_service(connection,id_group)
     return {"Group": teacher,"Students":students}, 200
+
+def delete_group_controller(current_user_id, group_id):
+    connection = db_connection()
+    if not connection:
+        return {"message": "Falha ao conectar com o banco de dados!"}, 500
+    try:
+        
+        if int(current_user_id["id"]) != Group.get_teacher_id_from_group_service(connection, group_id):
+            return {"message": "Sem permissão para deletar"}, 400
+        Group.delete_group_service(connection, group_id)
+        return {"message": "Grupo deletado com sucesso!"}, 200
+    except Exception as e:
+        return {"message": f"Erro ao deletar o grupo: {e}"}, 500
+    finally:
+        connection.close()
