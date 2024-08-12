3e100cbbc9bb0afb7db42b33ced9dc0b54b704f5
DaviMF29 <migueldavi313@gmail.com>
2024-08-12 14:57:37 -0300

    refactor: Update get_questions_from_teacher_controller to handle UTF-8 encoding errors

--- controllers/questions_controller.py
+++ controllers/questions_controller.py
@@ -3,11 +3,17 @@ from db.bd_mysql import db_connection
 from models.Questions import Questions
 
 
-def get_questions_from_teacher(conteudo, skill):
+def get_questions_from_teacher_controller(conteudo, skill):
     connection = db_connection()
 
     conteudo = conteudo.upper()
     skill = skill.upper()
+
+    try:
+        conteudo = conteudo.encode('utf-8').decode('utf-8')
+        skill = skill.encode('utf-8').decode('utf-8')
+    except UnicodeEncodeError as e:
+        return {"error": f"Erro de codificação UTF-8: {str(e)}"}, 400
     
     cabecalho, questions = Questions.get_questions_service_teacher(connection, conteudo, skill)
     if "error" in cabecalho:
--- routes/questions_routes.py
+++ routes/questions_routes.py
@@ -3,12 +3,10 @@ from flask import request, jsonify, Blueprint
 from controllers.questions_controller import *
 
 from flask_jwt_extended import jwt_required
-from flask_jwt_extended import get_jwt_identity
 
 question_app = Blueprint("question_app", __name__)
 
 @question_app.route("/api/question", methods=['GET'])
-@jwt_required()
 def get_questions_from_group_routes():
     conteudo = request.args.get('conteudo')
     skill = request.args.get('skill')
@@ -16,5 +14,11 @@ def get_questions_from_group_routes():
     if not conteudo or not skill:
         return jsonify({"error": "Parâmetros 'conteudo' e 'skill' são obrigatórios."}), 400
 
-    response, status_code = get_questions_from_teacher(conteudo, skill)
+    try:
+        conteudo = conteudo.encode('utf-8').decode('utf-8')
+        skill = skill.encode('utf-8').decode('utf-8')
+    except UnicodeEncodeError as e:
+        return jsonify({"error": f"Erro de codificação UTF-8: {str(e)}"}), 400
+
+    response, status_code = get_questions_from_teacher_controller(conteudo, skill)
     return jsonify(response), status_code
