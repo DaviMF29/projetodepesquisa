a375f700f8b5aea084e5555ea571db2e9750a750
giordanni <giordanni.formiga@aluno.uepb.edu.br>
2024-08-12 14:34:19 -0300

    Alterando o controller das questões

--- models/Questions.py
+++ models/Questions.py
@@ -4,73 +4,64 @@ from mysql.connector import Error
 
 
 class Questions:
-    def __init__(self, question, option_1, option_2, option_3, option_4, option_5, answer, group_id):
+    def __init__(self, id_questions, id_group_questions, level_questions, skill_question, question, option_a, option_b, option_c, option_d, answer):
+        self.id_questions = id_questions
+        self.id_group_questions = id_group_questions
+        self.level_questions = level_questions
+        self.skill_question = skill_question
         self.question = question
-        self.option_1 = option_1
-        self.option_2 = option_2
-        self.option_3 = option_3
-        self.option_4 = option_4
-        self.option_5 = option_5
+        self.option_1 = option_a
+        self.option_2 = option_b
+        self.option_3 = option_c
+        self.option_4 = option_d
         self.answer = answer
-        self.group_id = group_id
 
-
-    def create_questions_service(self, connection):
-        try: 
-            cursor = connection.cursor()
-            cursor.execute("INSERT INTO questions (question, option_1, option_2, option_3, option_4, option_5, answer, group_id) VALUES ( %s, %s, %s, %s, %s, %s, %s, %s)", 
-                           (self.question, self.option_1, self.option_2, self.option_3, self.option_4, self.option_5, self.answer, self.group_id))
-            connection.commit()
-            print("Questions saved successfully")
-            inserted_id = cursor.lastrowid
-            return inserted_id
-
-        except Error as e:
-            print(f"Error saving question to database: {e}")
-            return None
-
-        finally:
-            cursor.close()
 
     @staticmethod
-    def delete_questions_service(connection, title, id_grupo):
-        cursor = connection.cursor()
-        query = """DELETE q
-                    FROM questions q
-                    JOIN group_table g ON q.group_id = g.id_grupo
-                    WHERE g.title = %s AND g.id_grupo = %s
-        """
-        cursor.execute(query, (title, id_grupo))
-        connection.commit()
-        cursor.close()
-
-    @staticmethod
-    def get_questions_service_teacher(connection, title, groupId):
+    def get_questions_service_teacher(connection, conteudo, skill):
         with connection.cursor() as cursor:
-            select_query = """
-            SELECT q.question, q.option_1, q.option_2, q.option_3, q.option_4, q.option_5, q.answer, g.title
-            FROM questions q
-            JOIN group_table g 
-            ON q.group_id = g.id_grupo 
-            WHERE g.title = %s and g.id_grupo = %s"""
+            if skill == 'RECALLING':
+                query = """
+                SELECT  q.id_questions, q.level_questions, q.skill_question, g.content, 
+                        q.question, q.option_a, q.option_b, q.answer 
+                FROM questions q 
+                JOIN group_questions g ON q.id_group_questions = g.id_group_questions 
+                WHERE g.content = %s and q.skill_question = %s;
+                """
+            elif skill == 'RECOGNIZING':
+                query = """
+                SELECT  q.id_questions, q.level_questions, q.skill_question, g.content, 
+                        q.question, q.option_a, q.option_b, q.option_c, q.option_d, q.answer 
+                FROM questions q 
+                JOIN group_questions g ON q.id_group_questions = g.id_group_questions 
+                WHERE g.content = %s and q.skill_question = %s;
+                """
+            else:
+                return {"error": "Skill inválido fornecido."}, []
 
-            cursor.execute(select_query, (title, groupId,))
+            cursor.execute(query, (conteudo, skill,))
             results = cursor.fetchall()
 
-            quizzes = []
-            for row in results:
-                quiz = {
-                    "Question": row[0],
-                    "a)": row[1],
-                    "b)": row[2],
-                    "c)": row[3],
-                    "d)": row[4],
-                    "e)": row[5],
-                    "RESPOSTA:": row[6]
-                }
-                quizzes.append(quiz)
-            
-            return quizzes, title
+        if not results:
+            return {"error": "Nenhum resultado encontrado para os critérios fornecidos."}, []
 
-            
-        
+        cabecalho = {
+            "Level": results[0][1],
+            "Skill": results[0][2],
+            "Conteúdo": results[0][3],
+        }
+
+        questions = [
+            {
+                "ID": row[0],
+                "Question": row[4],
+                "a)": row[5],
+                "b)": row[6],
+                "c)": row[7] if len(row) > 7 else None,
+                "d)": row[8] if len(row) > 8 else None,
+                "RESPOSTA:": row[9]
+            }
+            for row in results
+        ]
+
+        return cabecalho, questions
