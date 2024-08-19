0dee5d5e2de17604e0d8c33d5c47e25edc8e928b
giordanni <giordanni.formiga@aluno.uepb.edu.br>
2024-08-19 15:34:43 -0300

    melhorando a tabela das questoes no banco

--- models/Questions.py
+++ models/Questions.py
@@ -4,41 +4,25 @@ from mysql.connector import Error
 
 
 class Questions:
-    def __init__(self, id_questions, id_group_questions, level_questions, skill_question, question, option_a, option_b, option_c, option_d, answer):
+    def __init__(self, id_questions, id_group_questions, level_questions, skill_question, question, answer):
         self.id_questions = id_questions
         self.id_group_questions = id_group_questions
         self.level_questions = level_questions
         self.skill_question = skill_question
         self.question = question
-        self.option_1 = option_a
-        self.option_2 = option_b
-        self.option_3 = option_c
-        self.option_4 = option_d
         self.answer = answer
 
 
     @staticmethod
     def get_questions_service_teacher(connection, conteudo, skill):
         with connection.cursor() as cursor:
-            if skill == 'RECALLING':
-                query = """
-                SELECT  q.id_questions, q.level_questions, q.skill_question, g.content, 
-                        q.question, q.option_a, q.option_b, q.answer 
-                FROM questions q 
-                JOIN group_questions g ON q.id_group_questions = g.id_group_questions 
-                WHERE g.content = %s and q.skill_question = %s;
-                """
-            elif skill == 'RECOGNIZING':
-                query = """
-                SELECT  q.id_questions, q.level_questions, q.skill_question, g.content, 
-                        q.question, q.option_a, q.option_b, q.option_c, q.option_d, q.answer 
-                FROM questions q 
-                JOIN group_questions g ON q.id_group_questions = g.id_group_questions 
-                WHERE g.content = %s and q.skill_question = %s;
-                """
-            else:
-                return {"error": "Skill inválido fornecido."}, []
-
+            query = """
+            SELECT  q.id_questions, q.level_questions, q.skill_question, g.content, 
+                    q.question,  q.answer 
+            FROM questions q 
+            JOIN group_questions g ON q.id_group_questions = g.id_group_questions 
+            WHERE g.content = %s and q.skill_question = %s;
+            """
             cursor.execute(query, (conteudo, skill,))
             results = cursor.fetchall()
 
@@ -55,13 +39,8 @@ class Questions:
             {
                 "ID": row[0],
                 "Question": row[4],
-                "a)": row[5],
-                "b)": row[6],
-                "c)": row[7] if len(row) > 7 else None,
-                "d)": row[8] if len(row) > 8 else None,
-                "RESPOSTA:": row[7]
+                "RESPOSTA:": row[5]
             }
             for row in results
         ]
-
         return cabecalho, questions
