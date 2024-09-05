a70e84306788cdf869753b39ff2d43c95efa3cd0
DaviMF29 <migueldavi313@gmail.com>
2024-09-05 14:27:48 -0300

    change parameter in delete_token_route

--- routes/token_routes.py
+++ routes/token_routes.py
@@ -4,11 +4,11 @@ from flask_jwt_extended import jwt_required
 
 token_app = Blueprint("token_app", __name__)
 
-@token_app.route('/api/token/<user_id>', methods=['DELETE'])
-def delete_token_route(user_id):
+@token_app.route('/api/token/<user_email>', methods=['DELETE'])
+def delete_token_route(user_email):
     token_type = request.args.get('token_type', None)
 
-    return delete_token_controller(user_id, token_type)
+    return delete_token_controller(user_email, token_type)
 
 @token_app.route('/api/token/groupid', methods=['GET'])
 def get_groupId_by_token_routes():
