552a800d8d34e1d4a4b86a249fc85d05c0979281
Giiordanni <jordani.alformiga@gmail.com>
2024-11-21 02:32:54 -0300

    importando get_jwt

--- routes/group_routes.py
+++ routes/group_routes.py
@@ -3,7 +3,7 @@ from flask import request, jsonify, Blueprint
 from controllers.group_controller import *
 
 from flask_jwt_extended import jwt_required
-from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import get_jwt_identity, get_jwt
 
 group_app = Blueprint("group_app", __name__)
 
